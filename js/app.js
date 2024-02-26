aa5280e9b9afc306aff85cceb57637dadce34df3
Juan Kerber <juanfke@hotmail.com>
2024-02-26 19:49:48 +0100

    deleted second camera

--- js/app.js
+++ js/app.js
@@ -103,36 +103,36 @@ myLink.onclick = function cameraon() {
     camera();
   }
 
-// function camera() {
-//     Dynamsoft.License.LicenseManager.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAyNjI3NzQwLVRYbFhaV0pRY205cSIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21kbHMuZHluYW1zb2Z0b25saW5lLmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAyNjI3NzQwIiwic3RhbmRieVNlcnZlclVSTCI6Imh0dHBzOi8vc2Rscy5keW5hbXNvZnRvbmxpbmUuY29tIiwiY2hlY2tDb2RlIjo0MjMxODUyNjB9");
-//     (async () => {
-//         let router = await Dynamsoft.CVR.CaptureVisionRouter.createInstance();
+function camera() {
+    Dynamsoft.License.LicenseManager.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAyNjI3NzQwLVRYbFhaV0pRY205cSIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21kbHMuZHluYW1zb2Z0b25saW5lLmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAyNjI3NzQwIiwic3RhbmRieVNlcnZlclVSTCI6Imh0dHBzOi8vc2Rscy5keW5hbXNvZnRvbmxpbmUuY29tIiwiY2hlY2tDb2RlIjo0MjMxODUyNjB9");
+    (async () => {
+        let router = await Dynamsoft.CVR.CaptureVisionRouter.createInstance();
     
-//         let view = await Dynamsoft.DCE.CameraView.createInstance();
-//         let cameraEnhancer = await Dynamsoft.DCE.CameraEnhancer.createInstance(view);
-//         document.querySelector("#cameraViewContainer").append(view.getUIElement());
-//         router.setInput(cameraEnhancer);
+        let view = await Dynamsoft.DCE.CameraView.createInstance();
+        let cameraEnhancer = await Dynamsoft.DCE.CameraEnhancer.createInstance(view);
+        document.querySelector("#cameraViewContainer").append(view.getUIElement());
+        router.setInput(cameraEnhancer);
     
-//         // const resultsContainer = document.querySelector("#results");
-//         router.addResultReceiver({ onDecodedBarcodesReceived: (result) => {
-//           if (result.barcodeResultItems.length > 0) {
-//             for (let item of result.barcodeResultItems) {
-//             //   resultsContainer.textContent += `${item.formatString}: ${item.text}\n\n`;
-//             isbntotable(item.text);
-//             }
-//           }
-//         }});
+        // const resultsContainer = document.querySelector("#results");
+        router.addResultReceiver({ onDecodedBarcodesReceived: (result) => {
+          if (result.barcodeResultItems.length > 0) {
+            for (let item of result.barcodeResultItems) {
+            //   resultsContainer.textContent += `${item.formatString}: ${item.text}\n\n`;
+            isbntotable(item.text);
+            }
+          }
+        }});
     
-//         let filter = new Dynamsoft.Utility.MultiFrameResultCrossFilter();
-//         filter.enableResultCrossVerification(
-//           Dynamsoft.Core.EnumCapturedResultItemType.CRIT_BARCODE, true
-//         );
-//         filter.enableResultDeduplication(
-//           Dynamsoft.Core.EnumCapturedResultItemType.CRIT_BARCODE, true
-//         );
-//         await router.addResultFilter(filter);
+        let filter = new Dynamsoft.Utility.MultiFrameResultCrossFilter();
+        filter.enableResultCrossVerification(
+          Dynamsoft.Core.EnumCapturedResultItemType.CRIT_BARCODE, true
+        );
+        filter.enableResultDeduplication(
+          Dynamsoft.Core.EnumCapturedResultItemType.CRIT_BARCODE, true
+        );
+        await router.addResultFilter(filter);
     
-//         await cameraEnhancer.open();
-//         await router.startCapturing("ReadSingleBarcode");
-//       })();
-// }
+        await cameraEnhancer.open();
+        await router.startCapturing("ReadSingleBarcode");
+      })();
+}
